f468d4e011aec43fc16aac2372a7d98b9c19b4b9
Vlad <Rubashkinvlad@yandex.ru>
2020-12-25 01:33:16 +0300

    complete day1 tasks

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        int x = 10;
+        while (x > 0){
+            System.out.print("JAVA ");
+            x--;
+        }
     }
 }
--- src/main/java/day1/Task3.java
+++ src/main/java/day1/Task3.java
@@ -2,6 +2,12 @@ package day1;
 
 public class Task3 {
     public static void main(String[] args) {
+        int x = 10;
+        while (x > 0){
+            System.out.println("JAVA");
+            x--;
+        }
+
 
     }
 }
