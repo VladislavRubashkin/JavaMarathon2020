a3a7e0703bf046648919a703c9f9e5826eefbe67
Vlad <Rubashkinvlad@yandex.ru>
2020-12-27 06:43:01 +0300

    complete day2 tasks

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,23 @@
 package day2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Введите количество этажей.");
+        int level = scanner.nextInt();
+        if (level >= 1 && level <= 4){
+            System.out.println("Малоэтажный дом");
+        }
+        else if(level >= 5 && level <= 8){
+            System.out.println("Среднеэтажный дом");
+        }
+        else if(level >= 9){
+            System.out.println("Многоэтажный дом");
+        }
+        else if( level <= 0)
+            System.out.println("Ошибка ввода");
 
     }
 }
