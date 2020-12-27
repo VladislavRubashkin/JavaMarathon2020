2d60da8c66acc039591c673e401a3fe9b1374699
Vlad <Rubashkinvlad@yandex.ru>
2020-12-27 10:55:47 +0300

    complete day3 tasks

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double number1;
+        double number2;
+        for (int i = 0; i < 5; i++) {
+            number1 = scanner.nextDouble();
+            number2 = scanner.nextDouble();
+            if(number2 == 0.0){
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(number1/number2);
+        }
     }
 }
