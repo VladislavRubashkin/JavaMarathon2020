413d402a05060dc10652d7c7f86aa2a48cb45127
Vlad <Rubashkinvlad@yandex.ru>
2021-01-03 17:48:04 +0300

    complete day5 tasks(странно делать проверку в конструкторе,но меня смутило то что Вы просите не использовать сеттеры

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,54 @@
 package day5;
 
 public class Task1 {
+    /*
+    Создать класс Автомобиль (англ. Car) с полями “Год выпуска”, “Цвет”, “Модель”. Создать get и set методы
+    для каждого поля. Создать экземпляр класса Автомобиль. Задать значение для каждого поля, используя set методы.
+    Вывести в консоль значение каждого из полей, используя get методы.
+    Созданный вами класс должен отвечать принципам инкапсуляции.
+     */
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setYear(1980);
+        car.setColor("Black");
+        car.setModel("BMW");
+        System.out.println("Год выпуска: " + car.getYear() + " Цвет: " + car.getColor() + " Модель: " + car.getModel());
     }
+}
+class Car{
+    private int year;
+    private String color;
+    private String model;
+
+    public void setYear(int year){
+        if (year <= 0){
+            System.out.println("Год выпуска не может быть меньше 0");
+        }else{
+            this.year = year;
+        }
+    }
+    public void setColor(String color){
+        if (color.isEmpty()){
+            System.out.println("Цвет должен быть указан");
+        }else{
+            this.color = color;
+        }
+    }
+    public void setModel(String model){
+        if (model.isEmpty()){
+            System.out.println("Модель должна быть указана");
+        }else{
+            this.model = model;
+        }
+    }
+    public int getYear(){
+        return year;
+    }
+    public String getColor(){
+        return color;
+    }
+    public String getModel(){
+        return model;
+    }
+
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -2,7 +2,48 @@ package day5;
 
 
 public class Task2 {
+    /*
+    Создать класс Мотоцикл (англ. Motorbike) с полями “Год выпуска”, “Цвет”, “Модель”.
+    Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать).
+    Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this.
+    Вывести в консоль значение каждого из полей, используя get методы.
+     */
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike(1998, "Red", "Honda");
+        System.out.println(motorbike.getYear() + " " + motorbike.getColor() + " " + motorbike.getModel());
 
     }
+}
+    class Motorbike{
+    private int year;
+    private String color;
+    private String model;
+
+    public Motorbike(int year, String color, String model){
+        if (year <= 0){
+            System.out.println("Год выпуска не может быть меньше 0");
+        }else {
+            this.year = year;
+        }
+        if (color.isEmpty()){
+            System.out.println("Цвет должен быть указан");
+        }else{
+            this.color = color;
+        }
+        if (model.isEmpty()){
+            System.out.println("Модель должна быть указана");
+        }else{
+            this.model = model;
+        }
+    }
+        public int getYear(){
+            return year;
+        }
+        public String getColor(){
+            return color;
+        }
+        public String getModel(){
+            return model;
+        }
+
 }
