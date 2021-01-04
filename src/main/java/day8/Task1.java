cb712e23fdfb80dbcd2aa25b4214a92316309497
Vlad <Rubashkinvlad@yandex.ru>
2021-01-04 21:34:35 +0300

    Complete day8 tasks

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,42 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        info();
     }
+    public static long measure() {
+        String s1 = " ";
+        long st, en;
+        st = System.nanoTime();
+        for (int i = 0; i <= 20000; i++) {
+            s1 += i + " ";
+        }
+        System.out.print(s1 + "\n");
+        en = System.nanoTime();
+        return en - st;
+    }
+    public static long measure2() {
+        StringBuilder sb = new StringBuilder();
+        long st, en;
+        st = System.nanoTime();
+        for (int i = 0; i <= 20000; i++) {
+            sb.append(i).append(" ");
+        }
+        System.out.print(sb.toString() + "\n");
+        en = System.nanoTime();
+        return en - st;
+    }
+
+    public static void info(){
+        System.out.println("---String--- cold start time " + String.format("%,12d",measure()) + " ns");
+        System.out.println();
+        System.out.println();
+        System.out.println("---String--- warmed JRE time " + String.format("%,12d",measure()) + " ns");
+        System.out.println();
+        System.out.println();
+        System.out.println("---StringBuilder--- cold start time " + String.format("%,12d",measure2()) + " ns");
+        System.out.println();
+        System.out.println();
+        System.out.println("---StringBuilder--- warmed JRE time " + String.format("%,12d",measure2()) + " ns");
+    }
+
 }
--- src/main/java/day8/Task2.java
+++ src/main/java/day8/Task2.java
@@ -2,6 +2,12 @@ package day8;
 
 public class Task2 {
     public static void main(String[] args) {
+        Airplane airplane = new Airplane ("Boeing", 2000, 150, 10000);
+        airplane.fillUp(2000);
+        System.out.println(airplane);
+        System.out.println();
+        airplane.info();
+
 
     }
 }
