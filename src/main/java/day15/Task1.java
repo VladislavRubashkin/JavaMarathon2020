778cc2a79eec62c20d61da2c5ef488dda93fc7ca
Vlad <Rubashkinvlad@yandex.ru>
2021-02-08 18:02:34 +0300

    Complete day 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,29 @@
 package day15;
 
+import java.io.*;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        File file = new File("src/main/resources/shoes.csv");
+        File file1 = new File("src/main/resources/missing_shoes.txt");
+
+        try {
+            Scanner scanner = new Scanner(file);
+            PrintWriter printWriter = new PrintWriter(file1);
+
+            while (scanner.hasNextLine()){
+                String[] shoes = scanner.nextLine().split(";");
+                if (Integer.parseInt(shoes[2]) == 0){
+                    printWriter.println(shoes[0] + " " + shoes[1] + " " + shoes[2]);
+                }
+            }
+            scanner.close();
+            printWriter.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
