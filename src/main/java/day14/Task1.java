d3665a8fa6debae678d53dacd0797a0414f8828e
Vlad <Rubashkinvlad@yandex.ru>
2021-02-08 03:01:58 +0300

    Complete day 14

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,53 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+
+        File fileRead = new File("file.txt");
+
+        printSumDigits(fileRead);
+
+    }
+
+    public static void printSumDigits(File file)  {
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            String line = scanner.nextLine();
+            String[] numbersString = line.split(" ");
+            int[] numbers = new int[10];
+
+            for (int i = 0; i < numbersString.length; i++) {
+                numbers[i] = Integer.parseInt(numbersString[i]);
+            }
+
+            if (numbersString.length != 10){
+                throw new IllegalArgumentException();
+            }
+
+            int sum = 0;
+            for (int number : numbers) {
+                sum += number;
+            }
+
+            System.out.println(sum);
+
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден.");
+        }catch (IllegalArgumentException e){
+            System.out.println("Некорректный входной файл");
+        }
+
+
+
 
     }
 }
--- src/main/java/day14/Task2.java
+++ src/main/java/day14/Task2.java
@@ -1,7 +1,46 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("people.txt");
+        System.out.println(parseFileToStringList(file));
+    }
 
+    public static List<String> parseFileToStringList(File file){
+
+
+        List<String> nameAndAge = new ArrayList<>();
+
+        try {
+           Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+
+                String[] person = line.split(" ");
+                int res = Integer.parseInt(person[1]);
+                if (res <= 0){
+                    throw new IllegalArgumentException();
+                }
+
+                nameAndAge.add(line);
+            }
+
+            return nameAndAge;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }catch (IllegalArgumentException e){
+            System.out.println("Некорректный входной файл");
+        }
+
+        return null;
     }
 }
