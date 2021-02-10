5614dabd84b517b9fe64b99698af817de2622ada
Vlad <Rubashkinvlad@yandex.ru>
2021-02-10 04:13:43 +0300

    Complete 16 day

--- src/main/java/day16/Task1.java
+++ src/main/java/day16/Task1.java
@@ -1,7 +1,40 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("file.txt");
+
+        printResult(file);
+
+    }
+
+    public static void printResult(File file){
+        int sum = 0;
+        double middle = 0.0;
+
+        try {
+            Scanner scanner = new Scanner(file);
+
+            String[] number = scanner.nextLine().split(" ");
+            double[] integer = new double[number.length];
+            for (int i = 0; i < integer.length; i++) {
+                    integer[i] = Double.parseDouble(number[i]);
+            }
+            for (double i : integer) {
+                sum += i;
+            }
+
+            middle = (double) sum/integer.length;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.printf(middle + " --> %.3f" , middle);
 
     }
 }
--- src/main/java/day16/Task2.java
+++ src/main/java/day16/Task2.java
@@ -1,7 +1,74 @@
 package day16;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
 
+        Random random = new Random();
+        int[] randomNumber = new int[1000];
+
+        for (int i = 0; i < randomNumber.length; i++) {
+            randomNumber[i] = random.nextInt(101);
+        }
+
+        File file1 = new File("src/main/resources/file1.txt");
+        File file2 = new File("src/main/resources/file2.txt");
+
+        try {
+            PrintWriter printWriter = new PrintWriter(file1);
+            for (int i : randomNumber) {
+                printWriter.println(i);
+            }
+            printWriter.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        recordingOfRealNumbers(file1, file2);
+        printResult(file2);
+    }
+
+    public static void recordingOfRealNumbers(File file1, File file2) {
+
+        try {
+            Scanner scanner = new Scanner(file1);
+            PrintWriter printWriter = new PrintWriter(file2);
+            int sum = 0;
+            int count = 0;
+
+            while (scanner.hasNextLine()) {
+                sum += Integer.parseInt(scanner.nextLine());
+                count++;
+
+                if (count == 20) {
+                    printWriter.println(sum / 20.0);
+                    sum = 0;
+                    count = 0;
+                }
+            }
+            printWriter.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printResult(File file){
+        double sum = 0;
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                sum += Double.parseDouble(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        System.out.println((int) sum);
     }
 }
