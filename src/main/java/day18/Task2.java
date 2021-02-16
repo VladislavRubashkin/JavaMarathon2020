a78db875a69356e58225506eef810ebf3d9e633a
Vlad <Rubashkinvlad@yandex.ru>
2021-02-17 02:34:40 +0300

    Complete day 18

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -1,7 +1,33 @@
 package day18;
 
+
 public class Task2 {
     public static void main(String[] args) {
 
+        int a = 787870077;
+        System.out.println(count7(a));
+    }
+    /*
+    787870077 / 10 = 78787007
+    78787007  / 10 = 7878700
+    7878700   / 10 = 787870
+    787870    / 10 = 78787
+    78787     / 10 = 7878
+    7878      / 10 = 787
+    787       / 10 = 78
+    78        / 10 = 7
+    7         / 10 = 0
+     */
+
+    public static int count7(int i){
+        if (i == 0){
+            return 0;
+        }
+
+        if (i % 10 == 7){
+            return 1 + count7(i / 10);
+        }
+        else
+            return count7(i / 10);
     }
 }
--- src/main/java/day18/Task3.java
+++ src/main/java/day18/Task3.java
@@ -3,5 +3,83 @@ package day18;
 public class Task3 {
     public static void main(String[] args) {
 
+         Node root = new Node(20);
+         int[] numbers = {14, 23, 11, 16, 22, 27, 5, 15, 18, 24, 150, 8};
+
+        for (int number : numbers) {
+            addNode(number, root);
+        }
+
+        dfs(root);
+
+    }
+
+    public static void addNode(int value, Node root){
+        if (root == null){
+            throw new IllegalArgumentException("Корень дерева не может быть пустым");
+        }
+        Node nextNode = root;
+        Node current = null;
+
+        // в цикле всегда делается по одной итерации(затем выход) -> на итерации проверяем в какой(левый
+        // или правый) узел дерева идти
+        while (nextNode != null){
+            current = nextNode;
+
+            if (value < current.getValue()){
+                nextNode = current.getNodeLeft();
+            }
+            else {
+                nextNode = current.getNodeRight();
+            }
+        }
+        // находимся на листочке и вставляем новый узел куда надо
+        if (value < current.getValue()){
+            current.setNodeLeft(new Node(value));
+        }
+        else
+            current.setNodeRight(new Node(value));
+    }
+
+    private static void dfs(Node root){
+        if (root == null){
+            return;
+        }
+        dfs(root.getNodeLeft());
+        System.out.print(root.getValue() + " ");
+        dfs(root.getNodeRight());
+    }
+}
+ class Node {
+    private int value;
+    private Node nodeLeft;  // по умолчанию null у созданного объекта
+    private Node nodeRight; // по умолчанию null у созданного объекта
+
+    public Node(int value){
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNodeLeft() {
+        return nodeLeft;
+    }
+
+    public void setNodeLeft(Node nodeLeft) {
+        this.nodeLeft = nodeLeft;
+    }
+
+    public Node getNodeRight() {
+        return nodeRight;
+    }
+
+    public void setNodeRight(Node nodeRight) {
+        this.nodeRight = nodeRight;
     }
 }
